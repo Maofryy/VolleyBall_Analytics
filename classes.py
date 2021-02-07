d4b857287f9007d811afb2465974b3a367f5242e
Maofryy <41508644+Maofryy@users.noreply.github.com>
2021-02-07 23:01:38 +0100

    Cleaned extract file, in classes are all object that might be useful for analysed data, focusing on parsing rn

---
+++ classes.py
@@ -0,0 +1,69 @@
+import json
+import tabula
+import pandas as pd
+
+class Set:
+    def __init__(self):
+        """ Init with zeros"""
+        #Basics        
+        self.team1 = ""
+        self.team2 = ""
+        self.subs1 = 0
+        self.subs2 = 0
+        self.serves1 = 0
+        self.serves2 = 0
+        self.timeout1 = 0
+        self.timeout2 = 0
+
+        #Analysed
+        self.team_serving = 0
+        self.start = 0.0
+        self.end = 0.0
+
+    def read_df(self, df):
+        """ read the datafram in arg into class parameters  """
+
+        self.team1 = df[0].columns[0]
+        self.team2 = df[1].columns[0]
+
+        """# Can store in time format but not needed
+        self.start = datetime.strptime(df[0].columns[1].split()[1], "%H:%M")
+        self.end = datetime.strptime(df[1].columns[1].split()[1], "%H:%M")
+        """
+
+        self.start = df[0].columns[1].split()[1]
+        self.end = df[1].columns[1].split()[1]
+
+        #if (df[0].columns[1].split()[2] == 'S'):
+        #    team_serving = self.team1
+        #else:
+        #    team_serving = self.team2        
+
+        self.subs1 = df[2]
+        self.subs2 = df[3]
+
+        self.serves1 = df[4]
+        self.serves2 = df[5]
+
+        self.timeout1 = df[6]
+        self.timeout2 = df[7] 
+    
+    def export_json(self, file):
+        """ export object as a json into the file name passed """
+        jsonStr = json.dumps(self.__dict__)
+        print(jsonStr)
+
+
+
+class Match:
+    def __init__(self, title, sets, teamA, teamB, referees, results, penalization):
+        """ Basic constructor """
+        self.title = title
+        self.sets = sets
+        self.teamA = teamA
+        self.teamB = teamB
+        self.referees = referees
+        self.results = results
+        self.penalization = penalization
+
+
--- extract_pdf.py
+++ extract_pdf.py
@@ -4,84 +4,15 @@ import json
 import pandas as pd
 #from datetime import datetime
 
-class Set:
-    def __init__(self):
-        #Basic parsing
-        self.team1 = ""
-        self.team2 = ""
-        self.subs1 = 0
-        self.subs2 = 0
-        self.serves1 = 0
-        self.serves2 = 0
-        self.timeout1 = 0
-        self.timeout2 = 0
-
-        #Analysed
-        self.team_serving = 0
-        self.start = 0.0
-        self.end = 0.0
-
-    def read_df(self, df):
-        self.team1 = df[0].columns[0]
-        self.team2 = df[1].columns[0]
-
-        """# Can store in time format but not needed
-        self.start = datetime.strptime(df[0].columns[1].split()[1], "%H:%M")
-        self.end = datetime.strptime(df[1].columns[1].split()[1], "%H:%M")
-        """
-
-        self.start = df[0].columns[1].split()[1]
-        self.end = df[1].columns[1].split()[1]
-
-        if (df[0].columns[1].split()[2] == 'S'):
-            team_serving = self.team1
-        else:
-            team_serving = self.team2        
-
-        self.subs1 = df[2]
-        self.subs2 = df[3]
-
-        self.serves1 = df[4]
-        self.serves2 = df[5]
-
-        self.timeout1 = df[6]
-        self.timeout2 = df[7] 
-        
-        """print(self.team1 + " vs " + self.team2)
-        print("from " + self.start + " to " + self.end)
-        print(team_serving + " to serve.")
-        print(self.subs1)
-        print(self.subs2)
-        print(self.serves1)
-        print(self.serves2)
-        print(self.timeout1)
-        print(self.timeout2)"""
-    
-    def export_json(self, file):
-        jsonStr = json.dumps(self.__dict__)
-        print(jsonStr)
-
-
-
-class Match:
-    def __init__(self, title, sets, teamA, teamB, referees, results, penalization):
-        self.title = title
-        self.sets = sets
-        self.teamA = teamA
-        self.teamB = teamB
-        self.referees = referees
-        self.results = results
-        self.penalization = penalization
-
 
 def extract_set(file, ref):
-
+    """ Reads a set the pdf in file at the passed ref point (top left point just after the "S E T X" column) """
     #Test to see if set isnt empty
     test_data = tabula.read_pdf(file, area=[[(ref[0] + 0.0), (ref[1] + 0.0), (ref[0] + 13.7), (ref[1] + 154.3)]], pages=1)
+    set_data = list()
     if ((not test_data) or (test_data[0].columns.size == 1)):
         print("Empty set at (" + str(ref[0]) + ", " + str(ref[1]) + ")")
-        return pd.DataFrame({label: []})
-    set_data = list()
+        return [0,0,0,0,0,0,0,0]
     # 0 : Team 1 (left)
     set_data.append(tabula.read_pdf(file, area=[(ref[0] + 0.0), (ref[1] + 0.0), (ref[0] + 13.7), (ref[1] + 154.3)], pages='1'))
     # 1 : Team 2
@@ -106,8 +37,8 @@ def extract_set(file, ref):
     set_data.append(tabula.read_pdf(file, area=[(ref[0] + 64.6), (ref[1] + 118.3 + team_diff), (ref[0] + 91.5), (ref[1] + 155 + team_diff)], pages='1'))
     
     # Flattening and cleaning set structure
-    set_data[0] = set_data[0][0]
-    set_data[1] = set_data[1][0]
+    set_data[0] = set_data[0][0].columns.values
+    set_data[1] = set_data[1][0].columns.values
 
     set_data[2] = set_data[2][0]
     set_data[3] = set_data[3][0]
@@ -126,49 +57,35 @@ def extract_set(file, ref):
     
     return set_data
 
-if __name__ == "__main__":
+    #def extract_team()
 
+if __name__ == "__main__":
+    
     ## Extract set data
     #Set 1 (73.4, 127.4)
     set1 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
-    #set1 = pd.DataFrame({'Set 1':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
-    #set1 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
     #print(set1)
 
     #Set 2 (73.4, 462.7)
-    set2 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
-    #set2 = pd.DataFrame({'Set 2':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
-    #set2 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    set2 = extract_set("ffvolley_fdme.php.pdf", (73.4, 462.7))
     #print(set2)
 
     #Set 3 (167, 128.2)
-    set3 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
-    #set3 = pd.DataFrame({'Set 3':[y})
-    #set3 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    set3 = extract_set("ffvolley_fdme.php.pdf", (167, 128.2))
     #print(set3)
 
     #Set 4 (167, 461.5)
-    set4 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
-    #set4 = pd.DataFrame({'Set 4':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
-    #set4 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    set4 = extract_set("ffvolley_fdme.php.pdf", (167, 461.5))
     #print(set4)
 
     #Set 5 (261.4, 28.1)
-    set5 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
-    #set5 = pd.DataFrame({'Set 5':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
-    #set5 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    set5 = extract_set("ffvolley_fdme.php.pdf", (261.4, 28.1))
     #print(set5)
 
 
-    """df_to_export = pd.DataFrame({'Sets':[[set1[0].columns.values, set1[1].columns.values, set1[2], set1[3], set1[4], set1[5], set1[6], set1[7]], 
-                                        [set2[0].columns.values, set2[1].columns.values, set2[2], set2[3], set2[4], set2[5], set2[6], set2[7]], 
-                                        [set3[0].columns.values, set3[1].columns.values, set3[2], set3[3], set3[4], set3[5], set3[6], set3[7]], 
-                                        [set4[0].columns.values, set4[1].columns.values, set4[2], set4[3], set4[4], set4[5], set4[6], set4[7]], 
-                                        [set5[0].columns.values, set5[1].columns.values, set5[2], set5[3], set5[4], set5[5], set5[6], set5[7]]]})"""
-    
     sets = pd.DataFrame({
-        'Team 1 ':[set1[0].columns.values, set2[0].columns.values, set3[0].columns.values, set4[0].columns.values, set5[0].columns.values],
-        'Team 2 ':[set1[1].columns.values, set2[1].columns.values, set3[1].columns.values, set4[1].columns.values, set5[1].columns.values],
+        'Team 1 ':[set1[0], set2[0], set3[0], set4[0], set5[0]],
+        'Team 2 ':[set1[1], set2[1], set3[1], set4[1], set5[1]],
         'Substitutions 1 ':[set1[2], set2[2], set3[2], set4[2], set5[2]],
         'Substitutions 2 ':[set1[3], set2[3], set3[3], set4[3], set5[3]],
         'Serves 1 ':[set1[4], set2[4], set3[1], set4[4], set5[4]],
@@ -178,11 +95,13 @@ if __name__ == "__main__":
                         })
     #sets = sets.rename(index={0:'Set 1', 1:'Set 2', 2:'Set 3', 3:'Set 4', 4:'Set 5'}, level=0)
     print(sets)
+    
     #Exporting all sets to Json
     json_output = sets.to_json()
-    print(json_output)
+    #print(json_output)
     with open("sets.json", 'w') as outfile:
         outfile.write(json_output)
+        print("JSON saved.")
 
     #Set 4 (empty test case) (167.8, 461.5)
     #set_data = extract_set("ffvolley_fdme.php.pdf", (167.8, 461.5))
