398acbe332c9431303ae7e0503de0a851f4fd727
Maofryy <41508644+Maofryy@users.noreply.github.com>
2021-02-07 19:29:13 +0100

    Extracting 5 sets (each has a row Set1=0 Set2=1 ...), Stopping the work on sets

--- extract_pdf.py
+++ extract_pdf.py
@@ -75,11 +75,12 @@ class Match:
 
 
 def extract_set(file, ref):
+
     #Test to see if set isnt empty
     test_data = tabula.read_pdf(file, area=[[(ref[0] + 0.0), (ref[1] + 0.0), (ref[0] + 13.7), (ref[1] + 154.3)]], pages=1)
-    if (test_data[0].columns.size == 1):
+    if ((not test_data) or (test_data[0].columns.size == 1)):
         print("Empty set at (" + str(ref[0]) + ", " + str(ref[1]) + ")")
-        return 0
+        return pd.DataFrame({label: []})
     set_data = list()
     # 0 : Team 1 (left)
     set_data.append(tabula.read_pdf(file, area=[(ref[0] + 0.0), (ref[1] + 0.0), (ref[0] + 13.7), (ref[1] + 154.3)], pages='1'))
@@ -117,27 +118,71 @@ def extract_set(file, ref):
     set_data[6] = set_data[6][0]
     set_data[7] = set_data[7][0]
     
-    df_to_export = pd.DataFrame({'Set 2':[set_data[0].columns, set_data[1].columns, set_data[2], set_data[3], set_data[4], set_data[5], set_data[6], set_data[7]]})
-    df_to_export = df_to_export.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
     
     """for dt in set_data:
         print(dt)"""
     #obj = Set()
     #obj.read_df(set_data)
     
-    return df_to_export
+    return set_data
 
-#Set 2 (73.4, 462.7)
-set2 = extract_set("ffvolley_fdme.php.pdf", (73.4, 462.7))
-#Exporting single set to Json
-json_output = set2.to_json()
-print(json_output)
-with open("set2.json", 'w') as outfile:
-    outfile.write(json_output)
+if __name__ == "__main__":
 
-#Set 4 (empty test case) (167.8, 461.5)
-#set_data = extract_set("ffvolley_fdme.php.pdf", (167.8, 461.5))
+    ## Extract set data
+    #Set 1 (73.4, 127.4)
+    set1 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
+    #set1 = pd.DataFrame({'Set 1':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
+    #set1 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    #print(set1)
 
-#ref = (73.4, 462.7)
-#col_size = 20
-#test_data = tabula.read_pdf("ffvolley_fdme.php.pdf", area=[(ref[0] + 14), (ref[1] + 0.0), (ref[0] + 57.0), (ref[1] + 118.3)], columns=[ref[1] +col_size*1, ref[1] + col_size*2, ref[1] + col_size*3, ref[1] + col_size*4, ref[1] + col_size*5, ref[1] + col_size*6], guess=False, pages=1)
+    #Set 2 (73.4, 462.7)
+    set2 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
+    #set2 = pd.DataFrame({'Set 2':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
+    #set2 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    #print(set2)
+
+    #Set 3 (167, 128.2)
+    set3 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
+    #set3 = pd.DataFrame({'Set 3':[y})
+    #set3 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    #print(set3)
+
+    #Set 4 (167, 461.5)
+    set4 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
+    #set4 = pd.DataFrame({'Set 4':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
+    #set4 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    #print(set4)
+
+    #Set 5 (261.4, 28.1)
+    set5 = extract_set("ffvolley_fdme.php.pdf", (73.4, 127.4))
+    #set5 = pd.DataFrame({'Set 5':[ret[0].columns.values, ret[1].columns.values, ret[2], ret[3], ret[4], ret[5], ret[6], ret[7]]})
+    #set5 = set1.rename(index={0:'Team 1', 1:'Team 2', 2:'Substitutions 1', 3:'Substitutions 2', 4:'Serves 1', 5:'Serves 2', 6:'Timeouts Team 1', 7:'Timeouts Team 2'})
+    #print(set5)
+
+
+    """df_to_export = pd.DataFrame({'Sets':[[set1[0].columns.values, set1[1].columns.values, set1[2], set1[3], set1[4], set1[5], set1[6], set1[7]], 
+                                        [set2[0].columns.values, set2[1].columns.values, set2[2], set2[3], set2[4], set2[5], set2[6], set2[7]], 
+                                        [set3[0].columns.values, set3[1].columns.values, set3[2], set3[3], set3[4], set3[5], set3[6], set3[7]], 
+                                        [set4[0].columns.values, set4[1].columns.values, set4[2], set4[3], set4[4], set4[5], set4[6], set4[7]], 
+                                        [set5[0].columns.values, set5[1].columns.values, set5[2], set5[3], set5[4], set5[5], set5[6], set5[7]]]})"""
+    
+    sets = pd.DataFrame({
+        'Team 1 ':[set1[0].columns.values, set2[0].columns.values, set3[0].columns.values, set4[0].columns.values, set5[0].columns.values],
+        'Team 2 ':[set1[1].columns.values, set2[1].columns.values, set3[1].columns.values, set4[1].columns.values, set5[1].columns.values],
+        'Substitutions 1 ':[set1[2], set2[2], set3[2], set4[2], set5[2]],
+        'Substitutions 2 ':[set1[3], set2[3], set3[3], set4[3], set5[3]],
+        'Serves 1 ':[set1[4], set2[4], set3[1], set4[4], set5[4]],
+        'Serves 2 ':[set1[5], set2[5], set3[5], set4[5], set5[5]],
+        'Timeouts 1 ':[set1[6], set2[6], set3[6], set4[6], set5[6]],
+        'Timeouts 2 ':[set1[7], set2[7], set3[7], set4[7], set5[7]],
+                        })
+    #sets = sets.rename(index={0:'Set 1', 1:'Set 2', 2:'Set 3', 3:'Set 4', 4:'Set 5'}, level=0)
+    print(sets)
+    #Exporting all sets to Json
+    json_output = sets.to_json()
+    print(json_output)
+    with open("sets.json", 'w') as outfile:
+        outfile.write(json_output)
+
+    #Set 4 (empty test case) (167.8, 461.5)
+    #set_data = extract_set("ffvolley_fdme.php.pdf", (167.8, 461.5))
